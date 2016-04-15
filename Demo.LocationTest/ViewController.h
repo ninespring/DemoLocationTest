906966912b8e99c0e019df7d0436ff969ed66957
Ninespring <xiaolongshen.stuff@gmail.com>
2016-04-15 14:26:13 +0800

    Initial Commit

---
+++ Demo.LocationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo.LocationTest
+//
+//  Created by Ninespring on 16/4/15.
+//  Copyright © 2016年 Ninespring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo.LocationTestTests/Demo_LocationTestTests.m
@@ -0,0 +1,39 @@
+//
+//  Demo_LocationTestTests.m
+//  Demo.LocationTestTests
+//
+//  Created by Ninespring on 16/4/15.
+//  Copyright © 2016年 Ninespring. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Demo_LocationTestTests : XCTestCase
+
+@end
+
+@implementation Demo_LocationTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
